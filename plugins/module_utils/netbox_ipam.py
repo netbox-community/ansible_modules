b72fa2a04b6601b8ed1f2fbf81672a9270310f9c
Mikhail Yohman <mikhail.yohman@gmail.com>
2020-01-31 21:06:04 +0000

    Check for address key before attempting to act on it

--- plugins/module_utils/netbox_ipam.py
+++ plugins/module_utils/netbox_ipam.py
@@ -146,10 +146,11 @@ class NetboxIpamModule(NetboxModule):
         data = self.data
 
         if self.endpoint == "ip_addresses":
-            try:
-                data["address"] = to_text(ipaddress.ip_network(data["address"]))
-            except ValueError:
-                pass
+            if data.key("address"):
+                try:
+                    data["address"] = to_text(ipaddress.ip_network(data["address"]))
+                except ValueError:
+                    pass
             name = data.get("address")
         elif self.endpoint in ["aggregates", "prefixes"]:
             name = data.get("prefix")
