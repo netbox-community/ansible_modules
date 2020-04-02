0917d36aec0659a5b29e1cb04b811d42a352b583
Douglas Heriot <douglas@douglasheriot.com>
2020-04-02 22:33:46 +1100

    Start creating unit tests for inventory plugin #56
    I've specifically started with tests for #140 new query_filters options and refresh_url

---
+++ tests/test_data.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2019, Bruno Inec (@sweenu) <bruno@inec.fr>
+# Copyright: (c) 2019, Mikhail Yohman (@FragmentedPacket) <mikhail.yohman@gmail.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+import json
+
+# Load test data from a json file, for a pytest parametrize
+def load_test_data(path, test_path):
+    with open(f"{path}/test_data/{test_path}/data.json", "r") as f:
+        data = json.loads(f.read())
+    tests = []
+    for test in data:
+        tuple_data = tuple(test.values())
+        tests.append(tuple_data)
+    return tests
+
