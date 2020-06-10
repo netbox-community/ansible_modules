b5cf8905e749f924cd5e9213e42456d355e6cd97
Mikhail Yohman <mikhail.yohman@gmail.com>
2020-06-10 11:12:28 -0600

    Enhancement: Added query_params to default arg_spec (#222)

--- plugins/module_utils/netbox_virtualization.py
+++ plugins/module_utils/netbox_virtualization.py
@@ -46,6 +46,7 @@ class NetboxVirtualizationModule(NetboxModule):
             application = self._find_app(self.endpoint)
         nb_app = getattr(self.nb, application)
         nb_endpoint = getattr(nb_app, self.endpoint)
+        user_query_params = self.module.params.get("query_params")
 
         data = self.data
 
@@ -59,7 +60,9 @@ class NetboxVirtualizationModule(NetboxModule):
             if not data.get("slug"):
                 data["slug"] = self._to_slug(name)
 
-        object_query_params = self._build_query_params(endpoint_name, data)
+        object_query_params = self._build_query_params(
+            endpoint_name, data, user_query_params
+        )
         self.nb_object = self._nb_endpoint_get(nb_endpoint, object_query_params, name)
 
         if self.state == "present":
