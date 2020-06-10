8cd7ceaa91b53dd5473e1633e181103ea1fb49f2
Mikhail Yohman <mikhail.yohman@gmail.com>
2020-06-09 20:12:13 -0600

    Merged netbox_utils with devel and removed old comments for to_text

--- plugins/module_utils/netbox_utils.py
+++ plugins/module_utils/netbox_utils.py
@@ -15,15 +15,12 @@ from itertools import chain
 
 from ansible_collections.ansible.netcommon.plugins.module_utils.compat import ipaddress
 
-# from ansible.module_utils._text import to_text
 from ansible.module_utils.common.text.converters import to_text
 
-# from ._text import to_native
 from ansible.module_utils._text import to_native
 from ansible.module_utils.common.collections import is_iterable
 from ansible.module_utils.basic import AnsibleModule, missing_required_lib
 from ansible.module_utils.urls import open_url
-from ansible.errors import AnsibleError
 
 PYNETBOX_IMP_ERR = None
 try:
