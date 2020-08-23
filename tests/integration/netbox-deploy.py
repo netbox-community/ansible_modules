bea6bb5012a0476d52fd9d3fca50b197f0d27e55
Douglas Heriot <douglas@douglasheriot.com>
2020-08-23 12:57:03 +1000

    Inventory: Create groups based on rack groups, and rack roles (#204)

--- tests/integration/netbox-deploy.py
+++ tests/integration/netbox-deploy.py
@@ -7,7 +7,11 @@ __metaclass__ = type
 import os
 import pynetbox
 
-# Set nb variable to connec to Netbox and use the veriable in future calls
+# NOTE: If anything depends on specific versions of NetBox, can check INTEGRATION_TESTS in env
+# os.environ["INTEGRATION_TESTS"]
+
+
+# Set nb variable to connect to Netbox and use the veriable in future calls
 nb = pynetbox.api("http://localhost:32768", "0123456789abcdef0123456789abcdef01234567")
 
 
@@ -159,22 +163,33 @@ created_device_roles = nb.dcim.device_roles.create(device_roles)
 core_switch = nb.dcim.device_roles.get(slug="core-switch")
 
 
-## Create Racks
-racks = [{"name": "Test Rack", "slug": "test-rack", "site": test_site2.id}]
-created_racks = nb.dcim.racks.create(racks)
-test_rack = nb.dcim.racks.get(slug="test-rack")
-
-
 ## Create Rack Groups
 rack_groups = [
-    {"name": "Test Rack Group", "slug": "test-rack-group", "site": test_site.id}
+    {"name": "Test Rack Group", "slug": "test-rack-group", "site": test_site.id},
+    {"name": "Parent Rack Group", "slug": "parent-rack-group", "site": test_site.id},
 ]
 created_rack_groups = nb.dcim.rack_groups.create(rack_groups)
 
+### Create Rack Group Parent relationship
+created_rack_groups[0].parent = created_rack_groups[1]
+created_rack_groups[0].save()
 
 ## Create Rack Roles
 rack_roles = [{"name": "Test Rack Role", "slug": "test-rack-role", "color": "4287f5"}]
 created_rack_roles = nb.dcim.rack_roles.create(rack_roles)
+
+## Create Racks
+racks = [
+    {
+        "name": "Test Rack Site 2",
+        "site": test_site2.id,
+        "role": created_rack_roles[0].id,
+    },
+    {"name": "Test Rack", "site": test_site.id, "group": created_rack_groups[0].id},
+]
+created_racks = nb.dcim.racks.create(racks)
+test_rack = nb.dcim.racks.get(name="Test Rack")  # racks don't have slugs
+test_rack_site2 = nb.dcim.racks.get(name="Test Rack Site 2")
 
 
 ## Create Devices
@@ -191,13 +206,14 @@ devices = [
         "device_type": cisco_test.id,
         "device_role": core_switch.id,
         "site": test_site.id,
+        "rack": test_rack.id,
     },
     {
         "name": "R1-Device",
         "device_type": cisco_test.id,
         "device_role": core_switch.id,
         "site": test_site2.id,
-        "rack": test_rack.id,
+        "rack": test_rack_site2.id,
     },
     {
         "name": "Test Nexus One",
@@ -316,7 +332,6 @@ created_virtual_machines_intfs = nb.virtualization.interfaces.create(
 
 
 ## Create Services
-
 services = [
     {"device": test100.id, "name": "ssh", "port": 22, "protocol": "tcp"},
     {
--- tests/integration/targets/inventory/runme.sh
+++ tests/integration/targets/inventory/runme.sh
@@ -1,29 +1,43 @@
 #!/usr/bin/env bash
 
-# Print commands as they're run
-set -x
+set -o xtrace # Print commands as they're run
+set -o errexit # abort on nonzero exitstatus
+set -o nounset # abort on unbound variable
+set -o pipefail # don't hide errors within pipes
 
 # Directory of this script
 SCRIPT_DIR="$( dirname "${BASH_SOURCE[0]}" )"
-
+RUNME_CONFIG="$SCRIPT_DIR/runme_config"
 INVENTORIES_DIR="$SCRIPT_DIR/files"
 
-COMPARE_OPTIONS=""
+# Load runme_config, if exists - the only way to pass environment when run through ansible-test
+if [[ -f "$RUNME_CONFIG" ]]
+then
+    source "$RUNME_CONFIG"
+fi
+
+declare -a COMPARE_OPTIONS # empty array
+
+# Check if NETBOX_VERSION has been set by runme_config, and if so, pass to compare_inventory_json.py
+if [[ "${NETBOX_VERSION:-}" == "v2.7" ]]
+then
+    COMPARE_OPTIONS+=(--netbox-version "${NETBOX_VERSION}")
+fi
 
 # OUTPUT_DIR is set by ansible-test
 # OUTPUT_INVENTORY_JSON is only set if running hacking/update_test_inventories.sh to update the test diff data
-if [ -n "$OUTPUT_INVENTORY_JSON" ]
+if [[ -n "${OUTPUT_INVENTORY_JSON:-}" ]]
 then
     OUTPUT_DIR="$OUTPUT_INVENTORY_JSON"
 
     # Clean up JSON fields we don't want to store and compare against in tests (creation times, etc.)
-    COMPARE_OPTIONS="--write"
+    COMPARE_OPTIONS+=(--write)
 fi
 
 echo OUTPUT_DIR="$OUTPUT_DIR"
 
 inventory () {
-    if [ -n "$OUTPUT_INVENTORY_JSON" ]
+    if [[ -n "${OUTPUT_INVENTORY_JSON:-}" ]]
     then
         # Running for the purpose of updating test data
         ansible-inventory "$@"
@@ -47,7 +61,7 @@ do
     inventory -vvvv --list --inventory "$INVENTORY" --output="$OUTPUT_JSON"
 
     # Compare the output
-    if ! "$SCRIPT_DIR/compare_inventory_json.py" $COMPARE_OPTIONS "$INVENTORIES_DIR/$NAME_WITHOUT_EXTENSION.json" "$OUTPUT_JSON"
+    if ! "$SCRIPT_DIR/compare_inventory_json.py" "${COMPARE_OPTIONS[@]}" "$INVENTORIES_DIR/$NAME_WITHOUT_EXTENSION.json" "$OUTPUT_JSON"
     then
         # Returned non-zero status
         RESULT=1
