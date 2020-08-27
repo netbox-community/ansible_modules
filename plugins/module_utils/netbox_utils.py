8f44607ca579fead04e7da2f1c5ced6199bdda42
Mikhail Yohman <mikhail.yohman@gmail.com>
2020-08-27 16:09:39 -0600

    Added back custom logic, but properly update object

--- plugins/module_utils/netbox_utils.py
+++ plugins/module_utils/netbox_utils.py
@@ -871,6 +871,9 @@ class NetboxModule(object):
         serialized_nb_obj = self.nb_object.serialize()
         updated_obj = serialized_nb_obj.copy()
         updated_obj.update(data)
+        if serialized_nb_obj.get("tags") and data.get("tags"):
+            serialized_nb_obj["tags"] = set(serialized_nb_obj["tags"])
+            updated_obj["tags"] = set(data["tags"])
 
         if serialized_nb_obj == updated_obj:
             return serialized_nb_obj, None
