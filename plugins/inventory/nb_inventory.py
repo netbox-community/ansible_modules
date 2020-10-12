ce121d5037d8805f4042866349718a9e4744b09d
Mikhail Yohman <mikhail.yohman@gmail.com>
2020-10-11 20:39:33 -0600

    Documentation: Added example of keyed_groups to nb_inventory (#361)

--- plugins/inventory/nb_inventory.py
+++ plugins/inventory/nb_inventory.py
@@ -212,6 +212,13 @@ compose:
   foo: last_updated
   bar: display_name
   nested_variable: rack.display_name
+
+# You can use keyed_groups to group on properties of devices or VMs.
+# NOTE: It's only possible to key off direct items on the device/VM objects.
+plugin: netbox.netbox.nb_inventory
+keyed_groups:
+  - prefix: status
+    key: status.value
 """
 
 import json
