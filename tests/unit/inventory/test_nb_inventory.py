c23a1bdb5022b6224d1763ce066e4e6c466ec1b9
nahun <nate.wheeler@gmail.com>
2020-12-28 22:22:18 -0800

    Update tests for VC members

--- tests/unit/inventory/test_nb_inventory.py
+++ tests/unit/inventory/test_nb_inventory.py
@@ -142,16 +142,24 @@ def test_refresh_lookups(inventory_fixture):
 
 
 @pytest.mark.parametrize(
-    "plurals, services, interfaces, dns_name, expected, not_expected",
+    "plurals, services, interfaces, dns_name, virtual_chassis_members, expected, not_expected",
     load_relative_test_data("group_extractors"),
 )
 def test_group_extractors(
-    inventory_fixture, plurals, services, interfaces, dns_name, expected, not_expected
+    inventory_fixture,
+    plurals,
+    services,
+    interfaces,
+    dns_name,
+    virtual_chassis_members,
+    expected,
+    not_expected,
 ):
     inventory_fixture.plurals = plurals
     inventory_fixture.services = services
     inventory_fixture.interfaces = interfaces
     inventory_fixture.dns_name = dns_name
+    inventory_fixture.virtual_chassis_members = virtual_chassis_members
     extractors = inventory_fixture.group_extractors
 
     for key in expected:
