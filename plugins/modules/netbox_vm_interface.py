febe59dc5ba98b88df165cc72e89f7b16370b455
ThomasADavis <tadavis@lbl.gov>
2021-10-06 12:21:42 -0700

    Allows the addition of a parent to an interface. (#604)
    * This allows the addition of the parent to an interface.
    
    Co-authored-by: Thomas Davis <tdavis@nersc.gov>

--- plugins/modules/netbox_vm_interface.py
+++ plugins/modules/netbox_vm_interface.py
@@ -82,6 +82,11 @@ options:
           - The mode of the interface
         required: false
         type: raw
+      parent_vm_interface:
+        description:
+          - the vm interface's parent interface.
+        required: false
+        type: raw
       untagged_vlan:
         description:
           - The untagged VLAN to be assigned to interface
@@ -208,6 +213,7 @@ def main():
                     mac_address=dict(required=False, type="str"),
                     description=dict(required=False, type="str"),
                     mode=dict(required=False, type="raw"),
+                    parent_vm_interface=dict(required=False, type="raw"),
                     untagged_vlan=dict(required=False, type="raw"),
                     tagged_vlans=dict(required=False, type="raw"),
                     tags=dict(required=False, type="list", elements="raw"),
