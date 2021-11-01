b600fb7ad58e5826d32560cf43a02a69355d4dae
Martin Rødvand <martin@rodvand.net>
2021-11-01 21:22:41 +0100

    Fixes #618 Ensure NetBox site module is idempotent between NetBox versions (#631)
    * Change latitude/longitude to float in Netbox Site
    * Change netbox_site to be idempotent between NetBox versions

--- plugins/module_utils/netbox_utils.py
+++ plugins/module_utils/netbox_utils.py
@@ -1144,6 +1144,21 @@ class NetboxModule(object):
             serialized_nb_obj["tags"] = set(serialized_nb_obj["tags"])
             updated_obj["tags"] = set(data["tags"])
 
+        # Ensure idempotency for site on version pre-3.0
+        version_pre_30 = self._version_check_greater("3.0", self.version)
+        if (
+            serialized_nb_obj.get("latitude")
+            and data.get("latitude")
+            and version_pre_30
+        ):
+            updated_obj["latitude"] = str(data["latitude"])
+        if (
+            serialized_nb_obj.get("longitude")
+            and data.get("longitude")
+            and version_pre_30
+        ):
+            updated_obj["longitude"] = str(data["longitude"])
+
         if serialized_nb_obj == updated_obj:
             return serialized_nb_obj, None
         else:
