791034ec39b2617937ee91a89cc5386a90b93816
Martin Rødvand <martin@rodvand.net>
2022-11-27 21:46:57 +0100

    Black

--- plugins/modules/netbox_inventory_item_role.py
+++ plugins/modules/netbox_inventory_item_role.py
@@ -124,7 +124,7 @@ def main():
                     name=dict(required=True, type="str"),
                     description=dict(required=False, type="str"),
                     color=dict(required=False, type="str"),
-                    slug=dict(required=False, type="str"),                    
+                    slug=dict(required=False, type="str"),
                     tags=dict(required=False, type="list", elements="raw"),
                     custom_fields=dict(required=False, type="dict"),
                 ),
