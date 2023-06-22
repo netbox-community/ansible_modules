5e97110dc23bcdd5ab2414c0f242d3f9a3671997
Philippe Morry <Philippe.Morry@orange.com>
2023-06-22 14:28:33 +0200

    pass black script

--- plugins/inventory/nb_inventory.py
+++ plugins/inventory/nb_inventory.py
@@ -2029,7 +2029,7 @@ class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
         self.ca_path = self.get_option("ca_path")
         if token:
             # add possibility to use Bearer token (JWT)
-            if "bearer" in token.lower():pass 
+            if "bearer" in token.lower():
                 self.headers.update(
                     {"Authorization": "Bearer %s" % token.split(" ", maxsplit=1)[1]}
                 )
