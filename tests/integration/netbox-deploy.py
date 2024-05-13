8cb1b7e31d6fa0750ac2ff593cc36d32b8b602ad
Martin Rødvand <martin@rodvand.net>
2024-05-13 21:47:36 +0200

    Deploy

--- tests/integration/netbox-deploy.py
+++ tests/integration/netbox-deploy.py
@@ -577,9 +577,9 @@ l2vpns = [
     },
 ]
 if nb_version >= version.parse("3.7"):
-    created_l2vpns = make_netbox_calls(nb.vpns.l2vpns, l2vpns)
+    created_l2vpns = make_netbox_calls(nb.vpn.l2vpns, l2vpns)
 else:
-    created_l2vpns = make_netbox_calls(nb.ipams.l2vpns, l2vpns)
+    created_l2vpns = make_netbox_calls(nb.ipam.l2vpns, l2vpns)
 
 if ERRORS:
     sys.exit(
