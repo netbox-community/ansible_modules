aa8d1057f686c30b35f170df1af43d4f9601cbdc
sapergus <94682748+sapergus@users.noreply.github.com>
2024-05-18 01:12:25 +0200

    added power outlet type iec-60320-c21 to template (#1232)

--- plugins/modules/netbox_power_outlet_template.py
+++ plugins/modules/netbox_power_outlet_template.py
@@ -50,6 +50,7 @@ options:
           - iec-60320-c13
           - iec-60320-c15
           - iec-60320-c19
+          - iec-60320-c21
           - iec-60309-p-n-e-4h
           - iec-60309-p-n-e-6h
           - iec-60309-p-n-e-9h
@@ -198,6 +199,7 @@ def main():
                             "iec-60320-c13",
                             "iec-60320-c15",
                             "iec-60320-c19",
+                            "iec-60320-c21",
                             "iec-60309-p-n-e-4h",
                             "iec-60309-p-n-e-6h",
                             "iec-60309-p-n-e-9h",
